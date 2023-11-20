a461c12166eb28d8e758e2472e62a10bdb5551f6
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-11-20 16:35:20 +0100

    code mort est parti

--- src/12_RefactoringGolf/hole1/kata.ts
+++ src/12_RefactoringGolf/hole1/kata.ts
@@ -126,11 +126,8 @@ class Board {
   }
 
   public AddTileAt(symbol: string, x: number, y: number): void {
-    //@ts-ignore
-    const tile: Tile = { X: x, Y: y, Symbol: symbol };
-
     this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
   }
 
-  
+
 }
