4b815f90c21996234e53fa6432f1fd311f32439f
guillaume <bertrandguillaume524@gmail.com>
2023-12-03 22:24:58 +0100

    Remplacement de l'interface par une classe tile, changement des méthodes tileAt et addTileAt pour utiliser les méthodes de la classe. Changement du constructeur pour créer l'instance de classe

--- src/12_RefactoringGolf/hole1/kata.ts
+++ src/12_RefactoringGolf/hole1/kata.ts
@@ -84,29 +84,57 @@ export class Game {
   }
 }
 
-interface Tile {
-  X: number;
-  Y: number;
-  Symbol: string;
+class Tile {
+  private x: number = 0;
+  private y: number = 0;
+  private symbol: string = ' ';
+
+  constructor(x: number, y: number, symbol: string) {
+    this.x = x;
+    this.y = y;
+    this.symbol = symbol;
+  }
+
+  get Symbol() {
+    return this.symbol;
+  }
+
+  get isNotEmpty() {
+    return this.Symbol !== emptyPlay;
+  }
+
+  hasSameSymbolAs(other: Tile) {
+    return this.Symbol === other.Symbol;
+  }
+
+  hasSameCoordinatesAs(other: Tile) {
+    return this.x == other.x && this.y == other.y;
+  }
+
+  updateSymbol(newSymbol: string) {
+    this.symbol = newSymbol;
+  }
 }
 
 class Board {
   private _plays: Tile[] = [];
 
   constructor() {
-    for (let i = firstRow; i <= thirdRow; i++) {
-      for (let j = firstColumn; j <= thirdColumn; j++) {
-        const tile: Tile = { X: i, Y: j, Symbol: emptyPlay };
-        this._plays.push(tile);
+    for (let x = firstRow; x <= thirdRow; x++) {
+      for (let y = firstColumn; y <= thirdColumn; y++) {
+        this._plays.push(new Tile(x, y, emptyPlay));
       }
     }
   }
 
   public TileAt(x: number, y: number): Tile {
-    return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
-  }
+    return this._plays.find((t: Tile) => t.hasSameCoordinatesAs(new Tile(x, y, emptyPlay)))!;
+  }  
 
   public AddTileAt(symbol: string, x: number, y: number): void {
-    this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
+    this._plays
+      .find((t: Tile) => t.hasSameCoordinatesAs(new Tile(x, y, symbol)))!
+      .updateSymbol(symbol);
   }
+  
 }
