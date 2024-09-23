56af40fc38939ad20d080d1b89a99ec84836a9c2
Sofia Gribanova <grisoffa@gmail.com>
2024-09-23 16:35:39 +0200

    delete duplication of code

--- src/12_RefactoringGolf/hole1/kata.ts
+++ src/12_RefactoringGolf/hole1/kata.ts
@@ -54,69 +54,35 @@ export class Game {
   }
 
   public Winner(): string {
-    if (this.isFirstRowFull() && this.isFirstRowFullWithSameSymbol()) {
+    if (this.isRowFull(0) && this.isRowFullWithSameSymbol(0)) {
       return this._board.TileAt(this.firstRow, this.firstColumn)!.Symbol;
     }
 
-    if (this.isSecondRowFull() && this.isSecondRowFullWithSameSymbol()) {
+    if (this.isRowFull(1) && this.isRowFullWithSameSymbol(1)) {
       return this._board.TileAt(this.secondRow, this.firstColumn)!.Symbol;
     }
 
-    if (this.isThirdRowFull() && this.isThirdRowFullWithSameSymbol()) {
+    if (this.isRowFull(2) && this.isRowFullWithSameSymbol(2)) {
       return this._board.TileAt(this.thirdRow, this.firstColumn)!.Symbol;
     }
 
     return this.emptyPlay;
   }
 
-  private isFirstRowFull() {
+  
+  private isRowFull(row: number) {
     return (
-      this._board.TileAt(this.firstRow, this.firstColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.firstRow, this.secondColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.firstRow, this.thirdColumn)!.Symbol != this.emptyPlay
+      this._board.TileAt(row, this.firstColumn)!.Symbol != this.emptyPlay &&
+      this._board.TileAt(row, this.secondColumn)!.Symbol != this.emptyPlay &&
+      this._board.TileAt(row, this.thirdColumn)!.Symbol != this.emptyPlay
     );
   }
-
-  private isFirstRowFullWithSameSymbol() {
+  private isRowFullWithSameSymbol(row: number) {
     return (
-      this._board.TileAt(this.firstRow, this.firstColumn)!.Symbol ==
-        this._board.TileAt(this.firstRow, this.secondColumn)!.Symbol &&
-      this._board.TileAt(this.firstRow, this.thirdColumn)!.Symbol ==
-        this._board.TileAt(this.firstRow, this.secondColumn)!.Symbol
-    );
-  }
-
-  private isSecondRowFull() {
-    return (
-      this._board.TileAt(this.secondRow, this.firstColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.secondRow, this.secondColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.secondRow, this.thirdColumn)!.Symbol != this.emptyPlay
-    );
-  }
-
-  private isSecondRowFullWithSameSymbol() {
-    return (
-      this._board.TileAt(this.secondRow, this.firstColumn)!.Symbol ==
-        this._board.TileAt(this.secondRow, this.secondColumn)!.Symbol &&
-      this._board.TileAt(this.secondRow, this.thirdColumn)!.Symbol ==
-        this._board.TileAt(this.secondRow, this.secondColumn)!.Symbol
-    );
-  }
-
-  private isThirdRowFull() {
-    return (
-      this._board.TileAt(this.thirdRow, this.firstColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.thirdRow, this.secondColumn)!.Symbol != this.emptyPlay &&
-      this._board.TileAt(this.thirdRow, this.thirdColumn)!.Symbol != this.emptyPlay
-    );
-  }
-
-  private isThirdRowFullWithSameSymbol() {
-    return (
-      this._board.TileAt(this.thirdRow, this.firstColumn)!.Symbol ==
-        this._board.TileAt(this.thirdRow, this.secondColumn)!.Symbol &&
-      this._board.TileAt(this.thirdRow, this.thirdColumn)!.Symbol ==
-        this._board.TileAt(this.thirdRow, this.secondColumn)!.Symbol
+      this._board.TileAt(row, this.firstColumn)!.Symbol ==
+        this._board.TileAt(row, this.secondColumn)!.Symbol &&
+      this._board.TileAt(row, this.thirdColumn)!.Symbol ==
+        this._board.TileAt(row, this.secondColumn)!.Symbol
     );
   }
 }
