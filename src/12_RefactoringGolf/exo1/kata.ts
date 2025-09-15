9536133af010aba36e43f275f07ef662481325dd
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2025-09-15 16:08:15 +0200

    Refactor: Extract row checking methods from Winner method to eliminate duplication

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -44,52 +44,39 @@ export class Game {
   }
 
   public Winner(): string {
-    //if the positions in first row are taken
-    if (
-      this._board.TileAt(0, 0)!.Symbol != " " &&
-      this._board.TileAt(0, 1)!.Symbol != " " &&
-      this._board.TileAt(0, 2)!.Symbol != " "
-    ) {
-      //if first row is full with same symbol
-      if (
-        this._board.TileAt(0, 0)!.Symbol == this._board.TileAt(0, 1)!.Symbol &&
-        this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol
-      ) {
-        return this._board.TileAt(0, 0)!.Symbol;
+    return this.checkRowWinner();
+  }
+
+  private checkRowWinner(): string {
+    for (let row = 0; row < 3; row++) {
+      const winner = this.checkRowWinnerAt(row);
+      if (winner != " ") {
+        return winner;
       }
     }
-
-    //if the positions in 2nd row are taken
-    if (
-      this._board.TileAt(1, 0)!.Symbol != " " &&
-      this._board.TileAt(1, 1)!.Symbol != " " &&
-      this._board.TileAt(1, 2)!.Symbol != " "
-    ) {
-      //if middle row is full with same symbol
-      if (
-        this._board.TileAt(1, 0)!.Symbol == this._board.TileAt(1, 1)!.Symbol &&
-        this._board.TileAt(1, 2)!.Symbol == this._board.TileAt(1, 1)!.Symbol
-      ) {
-        return this._board.TileAt(1, 0)!.Symbol;
-      }
-    }
-
-    //if the positions in 2nd row are taken
-    if (
-      this._board.TileAt(2, 0)!.Symbol != " " &&
-      this._board.TileAt(2, 1)!.Symbol != " " &&
-      this._board.TileAt(2, 2)!.Symbol != " "
-    ) {
-      //if middle row is full with same symbol
-      if (
-        this._board.TileAt(2, 0)!.Symbol == this._board.TileAt(2, 1)!.Symbol &&
-        this._board.TileAt(2, 2)!.Symbol == this._board.TileAt(2, 1)!.Symbol
-      ) {
-        return this._board.TileAt(2, 0)!.Symbol;
-      }
-    }
-
     return " ";
+  }
+
+  private checkRowWinnerAt(row: number): string {
+    if (this.isRowFull(row) && this.isRowSameSymbol(row)) {
+      return this._board.TileAt(row, 0)!.Symbol;
+    }
+    return " ";
+  }
+
+  private isRowFull(row: number): boolean {
+    return (
+      this._board.TileAt(row, 0)!.Symbol != " " &&
+      this._board.TileAt(row, 1)!.Symbol != " " &&
+      this._board.TileAt(row, 2)!.Symbol != " "
+    );
+  }
+
+  private isRowSameSymbol(row: number): boolean {
+    return (
+      this._board.TileAt(row, 0)!.Symbol == this._board.TileAt(row, 1)!.Symbol &&
+      this._board.TileAt(row, 2)!.Symbol == this._board.TileAt(row, 1)!.Symbol
+    );
   }
 }
 
