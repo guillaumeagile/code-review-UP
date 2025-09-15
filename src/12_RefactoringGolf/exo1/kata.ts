a44999dcced5da331d230dcd9c826b4a0b89b270
unkn0wnd.fbx <titouan.helbert@gmail.com>
2025-09-15 17:19:14 +0200

    exo1 works

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -2,109 +2,108 @@
 
 // read the code
 export class Game {
-private _lastSymbol = ' ';
-private _toto: Board = new Board();
+  private _lastSymbol = ' ';
+  private _toto: Board = new Board();
 
-public Play(symbol: string, x: number, y: number): void {
-//if first move
-if (this._lastSymbol == ' ') {
-  //if player is X
-  if (symbol == 'O') {
-    throw new Error('Invalid first player');
+  public Play(symbol: string, x: number, y: number): void {
+    this.validateFirstMove(symbol);
+    this.validatePlayerTurn(symbol);
+    this.validatePosition(x, y);
+
+    this._lastSymbol = symbol;
+    this._toto.AddTileAt(symbol, x, y);
   }
-}
-//if not first move but player repeated
-else if (symbol == this._lastSymbol) {
-  throw new Error('Invalid next player');
-}
-//if not first move but play on an already played tile
-else if (this._toto.TileAt(x, y).Symbol != ' ') {
-  throw new Error('Invalid position');
-}
 
-// update game state
-this._lastSymbol = symbol;
-this._toto.AddTileAt(symbol, x, y);
-}
-
-public Winner(): string {
-//if the positions in first row are taken
-if (
-  this._toto.TileAt(0, 0)!.Symbol != ' ' &&
-  this._toto.TileAt(0, 1)!.Symbol != ' ' &&
-  this._toto.TileAt(0, 2)!.Symbol != ' '
-) {
-  //if first row is full with same symbol
-  if (
-    this._toto.TileAt(0, 0)!.Symbol == this._toto.TileAt(0, 1)!.Symbol &&
-    this._toto.TileAt(0, 2)!.Symbol == this._toto.TileAt(0, 1)!.Symbol
-  ) {
-    return this._toto.TileAt(0, 0)!.Symbol;
+  private validateFirstMove(symbol: string): void {
+    if (this._lastSymbol == ' ') {
+      if (symbol == 'O') {
+        throw new Error('Invalid first player');
+      }
+    }
   }
-}
 
-//if the positions in 2nd row are taken
-if (
-  this._toto.TileAt(1, 0)!.Symbol != ' ' &&
-  this._toto.TileAt(1, 1)!.Symbol != ' ' &&
-  this._toto.TileAt(1, 2)!.Symbol != ' '
-) {
-  //if middle row is full with same symbol
-  if (
-    this._toto.TileAt(1, 0)!.Symbol == this._toto.TileAt(1, 1)!.Symbol &&
-    this._toto.TileAt(1, 2)!.Symbol == this._toto.TileAt(1, 1)!.Symbol
-  ) {
-    return this._toto.TileAt(1, 0)!.Symbol;
+  private validatePlayerTurn(symbol: string): void {
+    if (symbol == this._lastSymbol) {
+      throw new Error('Invalid next player');
+    }
   }
-}
 
-//if the positions in 2nd row are taken
-if (
-  this._toto.TileAt(2, 0)!.Symbol != ' ' &&
-  this._toto.TileAt(2, 1)!.Symbol != ' ' &&
-  this._toto.TileAt(2, 2)!.Symbol != ' '
-) {
-  //if middle row is full with same symbol
-  if (
-    this._toto.TileAt(2, 0)!.Symbol == this._toto.TileAt(2, 1)!.Symbol &&
-    this._toto.TileAt(2, 2)!.Symbol == this._toto.TileAt(2, 1)!.Symbol
-  ) {
-    return this._toto.TileAt(2, 0)!.Symbol;
+  private validatePosition(x: number, y: number): void {
+    if (this._toto.TileAt(x, y).Symbol != ' ') {
+      throw new Error('Invalid position');
+    }
   }
-}
 
-return ' ';
-}
+  public Winner(): string {
+    const firstRowWinner = this.checkRowWinner(0);
+    if (firstRowWinner !== ' ') {
+      return firstRowWinner;
+    }
+
+    const middleRowWinner = this.checkRowWinner(1);
+    if (middleRowWinner !== ' ') {
+      return middleRowWinner;
+    }
+
+    const bottomRowWinner = this.checkRowWinner(2);
+    if (bottomRowWinner !== ' ') {
+      return bottomRowWinner;
+    }
+
+    return ' ';
+  }
+
+  private checkRowWinner(row: number): string {
+    if (
+      this._toto.TileAt(row, 0)!.Symbol != ' ' &&
+      this._toto.TileAt(row, 1)!.Symbol != ' ' &&
+      this._toto.TileAt(row, 2)!.Symbol != ' '
+    ) {
+      if (
+        this._toto.TileAt(row, 0)!.Symbol == this._toto.TileAt(row, 1)!.Symbol &&
+        this._toto.TileAt(row, 2)!.Symbol == this._toto.TileAt(row, 1)!.Symbol
+      ) {
+        return this._toto.TileAt(row, 0)!.Symbol;
+      }
+    }
+    return ' ';
+  }
 }
 
 interface Tile {
-X: number;
-Y: number;
-Symbol: string;
+  X: number;
+  Y: number;
+  Symbol: string;
 }
 
 class Board {
-private _plays: Tile[] = [];
+  private _plays: Tile[] = [];
 
-constructor() {
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 3; j++) {
-    const tile: Tile = { X: i, Y: j, Symbol: ' ' };
-    this._plays.push(tile);
+  constructor() {
+    this.initializeBoard();
   }
-}
-}
 
-public TileAt(x: number, y: number): Tile {
-return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
-}
+  private initializeBoard(): void {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const tile: Tile = { X: i, Y: j, Symbol: ' ' };
+        this._plays.push(tile);
+      }
+    }
+  }
 
-public AddTileAt(symbol: string, x: number, y: number): void {
-//@ts-ignore
-const tile: Tile = { X: x, Y: y, Symbol: symbol };
+  public TileAt(x: number, y: number): Tile {
+    return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
+  }
 
-this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
-}
+  public AddTileAt(symbol: string, x: number, y: number): void {
+    const tile = this.findTileAt(x, y);
+    tile.Symbol = symbol;
+  }
+
+  private findTileAt(x: number, y: number): Tile {
+    return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
+  }
 }
 // create a PR,
 // fix indentation first
