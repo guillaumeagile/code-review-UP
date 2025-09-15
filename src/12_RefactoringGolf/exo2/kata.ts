64b6cda369b234f4e52b82037fab53c5a23fd003
Zeynnz <zzeynn8@gmail.com>
2025-09-15 16:22:09 +0200

    Groupe GRPB

--- src/12_RefactoringGolf/exo2/kata.ts
+++ src/12_RefactoringGolf/exo2/kata.ts
@@ -126,7 +126,6 @@ return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
 }
 
 public AddTileAt(symbol: string, x: number, y: number): void {     //@ts-ignore
-const tile: Tile = { X: x, Y: y, Symbol: symbol };
 this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
 }
 
