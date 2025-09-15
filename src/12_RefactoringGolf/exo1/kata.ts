85191c0beef08b76bfa7a9d5f4e2e555d323c0b4
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2025-09-15 16:11:30 +0200

    Refactor: Clean up Board class: remove unused method

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -103,14 +103,7 @@ class Board {
   }
 
   public AddTileAt(symbol: string, x: number, y: number): void {
-    //@ts-ignore
-    const tile: Tile = { X: x, Y: y, Symbol: symbol };
-
     this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
-  }
-
-  public determineFirstPlayer(): string {
-    throw new Error("Method not implemented.");
   }
 }
 // create a PR,
