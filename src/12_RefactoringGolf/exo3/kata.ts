c3a0bd82141942c38c66fe1eb5aa3baceef8f517
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-09-15 16:30:13 +0200

    Refacto

--- src/12_RefactoringGolf/exo3/kata.ts
+++ src/12_RefactoringGolf/exo3/kata.ts
@@ -1,6 +1,11 @@
-const leBonNom = " ";
+export const SYMBOLS = {
+  X: "X",
+  O: "O",
+  EMPTY: " "
+};
+
 export class Game {
-  private _lastSymbol = leBonNom;
+  private _lastSymbol = SYMBOLS.EMPTY;
   private _board: Board = new Board();
 
   public Play(symbol: string, x: number, y: number): void {
@@ -13,8 +18,8 @@ export class Game {
   }
 
   private validateFirstMove(player: string) {
-    if (this._lastSymbol == " ") {
-      if (player == "O") {
+    if (this._lastSymbol == SYMBOLS.EMPTY) {
+      if (player == SYMBOLS.O) {
         throw new Error("Invalid first player");
       }
     }
@@ -27,7 +32,7 @@ export class Game {
   }
 
   private validatePositionIsEmpty(x: number, y: number) {
-    if (this._board.TileAt(x, y).Symbol != " ") {
+    if (this._board.TileAt(x, y).Symbol != SYMBOLS.EMPTY) {
       throw new Error("Invalid position");
     }
   }
@@ -53,14 +58,14 @@ export class Game {
       return this._board.TileAt(2, 0)!.Symbol;
     }
 
-    return " ";
+    return SYMBOLS.EMPTY;
   }
 
   private isFirstRowFull() {
     return (
-      this._board.TileAt(0, 0)!.Symbol != " " &&
-      this._board.TileAt(0, 1)!.Symbol != " " &&
-      this._board.TileAt(0, 2)!.Symbol != " "
+      this._board.TileAt(0, 0)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(0, 1)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(0, 2)!.Symbol != SYMBOLS.EMPTY
     );
   }
 
@@ -73,9 +78,9 @@ export class Game {
 
   private isSecondRowFull() {
     return (
-      this._board.TileAt(1, 0)!.Symbol != " " &&
-      this._board.TileAt(1, 1)!.Symbol != " " &&
-      this._board.TileAt(1, 2)!.Symbol != " "
+      this._board.TileAt(1, 0)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(1, 1)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(1, 2)!.Symbol != SYMBOLS.EMPTY
     );
   }
 
@@ -88,9 +93,9 @@ export class Game {
 
   private isThirdRowFull() {
     return (
-      this._board.TileAt(2, 0)!.Symbol != " " &&
-      this._board.TileAt(2, 1)!.Symbol != " " &&
-      this._board.TileAt(2, 2)!.Symbol != " "
+      this._board.TileAt(2, 0)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(2, 1)!.Symbol != SYMBOLS.EMPTY &&
+      this._board.TileAt(2, 2)!.Symbol != SYMBOLS.EMPTY
     );
   }
 
@@ -114,7 +119,7 @@ class Board {
   constructor() {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        const tile: Tile = { X: i, Y: j, Symbol: " " };
+        const tile: Tile = { X: i, Y: j, Symbol: SYMBOLS.EMPTY };
         this._plays.push(tile);
       }
     }
--- src/12_RefactoringGolf/exo3/test.ts
+++ src/12_RefactoringGolf/exo3/test.ts
@@ -1,4 +1,4 @@
-import { Game } from "./kata";
+import { Game, SYMBOLS } from "./kata";
 
 describe("TicTacToe game", () => {
   let game: Game;
@@ -9,104 +9,104 @@ describe("TicTacToe game", () => {
 
   test("should not allow player O to play first", () => {
     expect(() => {
-      game.Play("O", 0, 0);
+      game.Play(SYMBOLS.O, 0, 0);
     }).toThrow();
   });
 
   it("should not allow player x to play twice in a row", () => {
-    game.Play("X", 0, 0);
+    game.Play(SYMBOLS.X, 0, 0);
     expect(() => {
-      game.Play("X", 1, 0);
+      game.Play(SYMBOLS.X, 1, 0);
     }).toThrow();
   });
 
   it("should not allow a player to play in last played position", () => {
-    game.Play("X", 0, 0);
+    game.Play(SYMBOLS.X, 0, 0);
     expect(() => {
-      game.Play("O", 0, 0);
+      game.Play(SYMBOLS.O, 0, 0);
     }).toThrow();
   });
 
   it("should not allow a player to play in any played position", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
+    game.Play(SYMBOLS.X, 0, 0);
+    game.Play(SYMBOLS.O, 1, 0);
     expect(() => {
-      game.Play("X", 0, 0);
+      game.Play(SYMBOLS.X, 0, 0);
     }).toThrow();
   });
 
   it("should declare player X as winner if it plays three in top row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
-    game.Play("X", 0, 1);
-    game.Play("O", 1, 1);
-    game.Play("X", 0, 2);
+    game.Play(SYMBOLS.X, 0, 0);
+    game.Play(SYMBOLS.O, 1, 0);
+    game.Play(SYMBOLS.X, 0, 1);
+    game.Play(SYMBOLS.O, 1, 1);
+    game.Play(SYMBOLS.X, 0, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("X");
+    expect(winner).toBe(SYMBOLS.X);
   });
 
   it("should declare player O as winner if it plays three in top row", () => {
-    game.Play("X", 1, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 2, 2);
-    game.Play("O", 0, 2);
+    game.Play(SYMBOLS.X, 1, 0);
+    game.Play(SYMBOLS.O, 0, 0);
+    game.Play(SYMBOLS.X, 1, 1);
+    game.Play(SYMBOLS.O, 0, 1);
+    game.Play(SYMBOLS.X, 2, 2);
+    game.Play(SYMBOLS.O, 0, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("O");
+    expect(winner).toBe(SYMBOLS.O);
   });
 
   it("should declare player X as winner if it plays three in middle row", () => {
-    game.Play("X", 1, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 1, 2);
+    game.Play(SYMBOLS.X, 1, 0);
+    game.Play(SYMBOLS.O, 0, 0);
+    game.Play(SYMBOLS.X, 1, 1);
+    game.Play(SYMBOLS.O, 0, 1);
+    game.Play(SYMBOLS.X, 1, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("X");
+    expect(winner).toBe(SYMBOLS.X);
   });
 
   it("should declare player O as winner if it plays three in middle row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
-    game.Play("X", 2, 1);
-    game.Play("O", 1, 1);
-    game.Play("X", 2, 2);
-    game.Play("O", 1, 2);
+    game.Play(SYMBOLS.X, 0, 0);
+    game.Play(SYMBOLS.O, 1, 0);
+    game.Play(SYMBOLS.X, 2, 1);
+    game.Play(SYMBOLS.O, 1, 1);
+    game.Play(SYMBOLS.X, 2, 2);
+    game.Play(SYMBOLS.O, 1, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("O");
+    expect(winner).toBe(SYMBOLS.O);
   });
 
   it("should declare player X as winner if it plays three in bottom row", () => {
-    game.Play("X", 2, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 2, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 2, 2);
+    game.Play(SYMBOLS.X, 2, 0);
+    game.Play(SYMBOLS.O, 0, 0);
+    game.Play(SYMBOLS.X, 2, 1);
+    game.Play(SYMBOLS.O, 0, 1);
+    game.Play(SYMBOLS.X, 2, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("X");
+    expect(winner).toBe(SYMBOLS.X);
   });
 
   it("should declare player O as winner if it plays three in bottom row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 2, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 2, 1);
-    game.Play("X", 0, 1);
-    game.Play("O", 2, 2);
+    game.Play(SYMBOLS.X, 0, 0);
+    game.Play(SYMBOLS.O, 2, 0);
+    game.Play(SYMBOLS.X, 1, 1);
+    game.Play(SYMBOLS.O, 2, 1);
+    game.Play(SYMBOLS.X, 0, 1);
+    game.Play(SYMBOLS.O, 2, 2);
 
     const winner = game.Winner();
 
-    expect(winner).toBe("O");
+    expect(winner).toBe(SYMBOLS.O);
   });
 });
