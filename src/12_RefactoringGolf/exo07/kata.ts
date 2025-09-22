db10aa3371ce8e4c06d035154fb22abea9f633be
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-09-22 16:49:51 +0200

    Refactoring 2

--- src/12_RefactoringGolf/exo07/kata.ts
+++ src/12_RefactoringGolf/exo07/kata.ts
@@ -12,13 +12,13 @@ export class Game {
   private _lastPlayer = noPlayer;
   private _board: Board = new Board();
 
-  public Play(player: string, x: number, y: number): void {
-    this.validateFirstMove(player);
-    this.validatePlayer(player);
-    this.validatePositionIsEmpty(x, y);
+  public Play(tile: Tile): void {
+    this.validateFirstMove(tile.Player);
+    this.validatePlayer(tile.Player);
+    this.validatePositionIsEmpty(tile.Coordinates);
 
-    this.updateLastPlayer(player);
-    this.updateBoard(new Tile(x, y, player));
+    this.updateLastPlayer(tile.Player);
+    this.updateBoard(tile);
   }
 
   private validateFirstMove(player: string) {
@@ -35,8 +35,8 @@ export class Game {
     }
   }
 
-  private validatePositionIsEmpty(x: number, y: number) {
-    if (this._board.isTilePlayedAt(x, y)) {
+  private validatePositionIsEmpty(coordinates: Coordinates) {
+    if (this._board.isTilePlayedAt(coordinates)) {
       throw new Error("Invalid position");
     }
   }
@@ -54,19 +54,39 @@ export class Game {
   }
 }
 
-class Tile {
+export class Coordinates {
   private x: number = 0;
   private y: number = 0;
-  private player: string = noPlayer;
 
-  constructor(x: number, y: number, player: string) {
+  constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
+  }
+
+  get X() {
+    return this.x;
+  }
+
+  get Y() {
+    return this.y;
+  }
+}
+
+export class Tile {
+  private coordinates: Coordinates;
+  private player: string = noPlayer;
+
+  constructor(coordinates: Coordinates, player: string) {
+    this.coordinates = coordinates;
     this.player = player;
   }
 
   get Player() {
     return this.player;
+  }
+
+  get Coordinates() {
+    return this.coordinates;
   }
 
   get isNotEmpty() {
@@ -78,7 +98,7 @@ class Tile {
   }
 
   hasSameCoordinatesAs(other: Tile) {
-    return this.x == other.x && this.y == other.y;
+    return this.coordinates.X == other.coordinates.X && this.coordinates.Y == other.coordinates.Y;
   }
 
   updatePlayer(newPlayer: string) {
@@ -92,14 +112,14 @@ class Board {
   constructor() {
     for (let x = firstRow; x <= thirdRow; x++) {
       for (let y = firstColumn; y <= thirdColumn; y++) {
-        this._plays.push(new Tile(x, y, noPlayer));
+        this._plays.push(new Tile(new Coordinates(x, y), noPlayer));
       }
     }
   }
 
-  public isTilePlayedAt(x: number, y: number) {
+  public isTilePlayedAt(coordinates: Coordinates) {
     return this._plays.find((t: Tile) =>
-      t.hasSameCoordinatesAs(new Tile(x, y, noPlayer)),
+      t.hasSameCoordinatesAs(new Tile(new Coordinates(coordinates.X, coordinates.Y), noPlayer)),
     )!.isNotEmpty;
   }
 
@@ -111,46 +131,46 @@ class Board {
 
   public findRowFullWithSamePlayer(): string {
     if (this.isRowFull(firstRow) && this.isRowFullWithSamePlayer(firstRow)) {
-      return this.playerAt(firstRow, firstColumn);
+      return this.playerAt(new Coordinates(firstRow, firstColumn));
     }
 
     if (this.isRowFull(secondRow) && this.isRowFullWithSamePlayer(secondRow)) {
-      return this.playerAt(secondRow, firstColumn);
+      return this.playerAt(new Coordinates(secondRow, firstColumn));
     }
 
     if (this.isRowFull(thirdRow) && this.isRowFullWithSamePlayer(thirdRow)) {
-      return this.playerAt(thirdRow, firstColumn);
+      return this.playerAt(new Coordinates(thirdRow, firstColumn));
     }
 
     return noPlayer;
   }
 
-  private hasSamePlayer(x: number, y: number, otherX: number, otherY: number) {
-    return this.TileAt(x, y)!.hasSamePlayerAs(this.TileAt(otherX, otherY)!);
+  private hasSamePlayer(coordinates: Coordinates, otherCoordinates: Coordinates) {
+    return this.TileAt(coordinates)!.hasSamePlayerAs(this.TileAt(otherCoordinates)!);
   }
 
-  private playerAt(x: number, y: number) {
-    return this.TileAt(x, y)!.Player;
+  private playerAt(coordinates: Coordinates) {
+    return this.TileAt(coordinates)!.Player;
   }
 
-  private TileAt(x: number, y: number): Tile {
+  private TileAt(coordinates: Coordinates): Tile {
     return this._plays.find((t: Tile) =>
-      t.hasSameCoordinatesAs(new Tile(x, y, noPlayer)),
+      t.hasSameCoordinatesAs(new Tile(coordinates, noPlayer)),
     )!;
   }
 
   private isRowFull(row: number) {
     return (
-      this.isTilePlayedAt(row, firstColumn) &&
-      this.isTilePlayedAt(row, secondColumn) &&
-      this.isTilePlayedAt(row, thirdColumn)
+      this.isTilePlayedAt(new Coordinates(row, firstColumn)) &&
+      this.isTilePlayedAt(new Coordinates(row, secondColumn)) &&
+      this.isTilePlayedAt(new Coordinates(row, thirdColumn))
     );
   }
 
   private isRowFullWithSamePlayer(row: number) {
     return (
-      this.hasSamePlayer(row, firstColumn, row, secondColumn) &&
-      this.hasSamePlayer(row, secondColumn, row, thirdColumn)
+      this.hasSamePlayer(new Coordinates(row, firstColumn), new Coordinates(row, secondColumn)) &&
+      this.hasSamePlayer(new Coordinates(row, secondColumn), new Coordinates(row, thirdColumn))
     );
   }
 }
--- src/12_RefactoringGolf/exo07/test.ts
+++ src/12_RefactoringGolf/exo07/test.ts
@@ -1,4 +1,4 @@
-import { Game } from "./kata";
+import { Game, Coordinates, Tile } from "./kata";
 
 describe("TicTacToe game", () => {
   let game: Game;
@@ -9,38 +9,38 @@ describe("TicTacToe game", () => {
 
   test("should not allow player O to play first", () => {
     expect(() => {
-      game.Play("O", 0, 0);
+      game.Play(new Tile(new Coordinates(0, 0), "O"));
     }).toThrow();
   });
 
   it("should not allow player x to play twice in a row", () => {
-    game.Play("X", 0, 0);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
     expect(() => {
-      game.Play("X", 1, 0);
+      game.Play(new Tile(new Coordinates(1, 0), "X"));
     }).toThrow();
   });
 
   it("should not allow a player to play in last played position", () => {
-    game.Play("X", 0, 0);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
     expect(() => {
-      game.Play("O", 0, 0);
+      game.Play(new Tile(new Coordinates(0, 0), "O"));
     }).toThrow();
   });
 
   it("should not allow a player to play in any played position", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
+    game.Play(new Tile(new Coordinates(1, 0), "O"));
     expect(() => {
-      game.Play("X", 0, 0);
+      game.Play(new Tile(new Coordinates(0, 0), "X"));
     }).toThrow();
   });
 
   it("should declare player X as winner if it plays three in top row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
-    game.Play("X", 0, 1);
-    game.Play("O", 1, 1);
-    game.Play("X", 0, 2);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
+    game.Play(new Tile(new Coordinates(1, 0), "O"));
+    game.Play(new Tile(new Coordinates(0, 1), "X"));
+    game.Play(new Tile(new Coordinates(1, 1), "O"));
+    game.Play(new Tile(new Coordinates(0, 2), "X"));
 
     const winner = game.Winner();
 
@@ -48,12 +48,12 @@ describe("TicTacToe game", () => {
   });
 
   it("should declare player O as winner if it plays three in top row", () => {
-    game.Play("X", 1, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 2, 2);
-    game.Play("O", 0, 2);
+    game.Play(new Tile(new Coordinates(1, 0), "X"));
+    game.Play(new Tile(new Coordinates(0, 0), "O"));
+    game.Play(new Tile(new Coordinates(1, 1), "X"));
+    game.Play(new Tile(new Coordinates(0, 1), "O"));
+    game.Play(new Tile(new Coordinates(2, 2), "X"));
+    game.Play(new Tile(new Coordinates(0, 2), "O"));
 
     const winner = game.Winner();
 
@@ -61,11 +61,11 @@ describe("TicTacToe game", () => {
   });
 
   it("should declare player X as winner if it plays three in middle row", () => {
-    game.Play("X", 1, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 1, 2);
+    game.Play(new Tile(new Coordinates(1, 0), "X"));
+    game.Play(new Tile(new Coordinates(0, 0), "O"));
+    game.Play(new Tile(new Coordinates(1, 1), "X"));
+    game.Play(new Tile(new Coordinates(0, 1), "O"));
+    game.Play(new Tile(new Coordinates(1, 2), "X"));
 
     const winner = game.Winner();
 
@@ -73,12 +73,12 @@ describe("TicTacToe game", () => {
   });
 
   it("should declare player O as winner if it plays three in middle row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 1, 0);
-    game.Play("X", 2, 1);
-    game.Play("O", 1, 1);
-    game.Play("X", 2, 2);
-    game.Play("O", 1, 2);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
+    game.Play(new Tile(new Coordinates(1, 0), "O"));
+    game.Play(new Tile(new Coordinates(2, 1), "X"));
+    game.Play(new Tile(new Coordinates(1, 1), "O"));
+    game.Play(new Tile(new Coordinates(2, 2), "X"));
+    game.Play(new Tile(new Coordinates(1, 2), "O"));
 
     const winner = game.Winner();
 
@@ -86,11 +86,11 @@ describe("TicTacToe game", () => {
   });
 
   it("should declare player X as winner if it plays three in bottom row", () => {
-    game.Play("X", 2, 0);
-    game.Play("O", 0, 0);
-    game.Play("X", 2, 1);
-    game.Play("O", 0, 1);
-    game.Play("X", 2, 2);
+    game.Play(new Tile(new Coordinates(2, 0), "X"));
+    game.Play(new Tile(new Coordinates(0, 0), "O"));
+    game.Play(new Tile(new Coordinates(2, 1), "X"));
+    game.Play(new Tile(new Coordinates(0, 1), "O"));
+    game.Play(new Tile(new Coordinates(2, 2), "X"));
 
     const winner = game.Winner();
 
@@ -98,12 +98,12 @@ describe("TicTacToe game", () => {
   });
 
   it("should declare player O as winner if it plays three in bottom row", () => {
-    game.Play("X", 0, 0);
-    game.Play("O", 2, 0);
-    game.Play("X", 1, 1);
-    game.Play("O", 2, 1);
-    game.Play("X", 0, 1);
-    game.Play("O", 2, 2);
+    game.Play(new Tile(new Coordinates(0, 0), "X"));
+    game.Play(new Tile(new Coordinates(2, 0), "O"));
+    game.Play(new Tile(new Coordinates(1, 1), "X"));
+    game.Play(new Tile(new Coordinates(2, 1), "O"));
+    game.Play(new Tile(new Coordinates(0, 1), "X"));
+    game.Play(new Tile(new Coordinates(2, 2), "O"));
 
     const winner = game.Winner();
 
