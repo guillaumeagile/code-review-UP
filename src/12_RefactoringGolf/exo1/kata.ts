405bf5adbed3ed2df5d7482c31f8567d03d2b70f
ElAbdos <153218250+ElAbdos@users.noreply.github.com>
2025-09-30 08:30:34 +0200

    suppression des commentaires inutiles et ou évident .

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -1,40 +1,30 @@
 /* eslint-disable */
 
-// read the code
 export class Game {
     private _lastSymbol = ' ';
     private _toto: Board = new Board();
 
     public Play(symbol: string, x: number, y: number): void {
-//if first move
         if (this._lastSymbol == ' ') {
-            //if player is X
             if (symbol == 'O') {
                 throw new Error('Invalid first player');
             }
-        }
-//if not first move but player repeated
-        else if (symbol == this._lastSymbol) {
+        } else if (symbol == this._lastSymbol) {
             throw new Error('Invalid next player');
-        }
-//if not first move but play on an already played tile
-        else if (this._toto.TileAt(x, y).Symbol != ' ') {
+        } else if (this._toto.TileAt(x, y).Symbol != ' ') {
             throw new Error('Invalid position');
         }
 
-// update game state
         this._lastSymbol = symbol;
         this._toto.AddTileAt(symbol, x, y);
     }
 
     public Winner(): string {
-//if the positions in first row are taken
         if (
             this._toto.TileAt(0, 0)!.Symbol != ' ' &&
             this._toto.TileAt(0, 1)!.Symbol != ' ' &&
             this._toto.TileAt(0, 2)!.Symbol != ' '
         ) {
-            //if first row is full with same symbol
             if (
                 this._toto.TileAt(0, 0)!.Symbol == this._toto.TileAt(0, 1)!.Symbol &&
                 this._toto.TileAt(0, 2)!.Symbol == this._toto.TileAt(0, 1)!.Symbol
@@ -43,13 +33,11 @@ export class Game {
             }
         }
 
-//if the positions in 2nd row are taken
         if (
             this._toto.TileAt(1, 0)!.Symbol != ' ' &&
             this._toto.TileAt(1, 1)!.Symbol != ' ' &&
             this._toto.TileAt(1, 2)!.Symbol != ' '
         ) {
-            //if middle row is full with same symbol
             if (
                 this._toto.TileAt(1, 0)!.Symbol == this._toto.TileAt(1, 1)!.Symbol &&
                 this._toto.TileAt(1, 2)!.Symbol == this._toto.TileAt(1, 1)!.Symbol
@@ -58,13 +46,11 @@ export class Game {
             }
         }
 
-//if the positions in 2nd row are taken
         if (
             this._toto.TileAt(2, 0)!.Symbol != ' ' &&
             this._toto.TileAt(2, 1)!.Symbol != ' ' &&
             this._toto.TileAt(2, 2)!.Symbol != ' '
         ) {
-            //if middle row is full with same symbol
             if (
                 this._toto.TileAt(2, 0)!.Symbol == this._toto.TileAt(2, 1)!.Symbol &&
                 this._toto.TileAt(2, 2)!.Symbol == this._toto.TileAt(2, 1)!.Symbol
@@ -100,16 +86,9 @@ class Board {
     }
 
     public AddTileAt(symbol: string, x: number, y: number): void {
-//@ts-ignore
+        //@ts-ignore
         const tile: Tile = {X: x, Y: y, Symbol: symbol};
 
         this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
     }
 }
-
-// create a PR,
-// fix indentation first
-//  commit and push
-// make your comments,
-// then refactor
-// submit your PR for review
