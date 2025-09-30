aa7026a6e73be46cdea87bb7c73983acbf792d3b
ElAbdos <153218250+ElAbdos@users.noreply.github.com>
2025-09-30 08:42:02 +0200

    refactor la vérification des lignes winner dans une boucle

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -20,45 +20,15 @@ export class Game {
     }
 
     public Winner(): string {
-        if (
-            this._toto.TileAt(0, 0)!.Symbol != ' ' &&
-            this._toto.TileAt(0, 1)!.Symbol != ' ' &&
-            this._toto.TileAt(0, 2)!.Symbol != ' '
-        ) {
+        for (let row = 0; row < 3; row++) {
             if (
-                this._toto.TileAt(0, 0)!.Symbol == this._toto.TileAt(0, 1)!.Symbol &&
-                this._toto.TileAt(0, 2)!.Symbol == this._toto.TileAt(0, 1)!.Symbol
+                this._toto.TileAt(row, 0).Symbol !== ' ' &&
+                this._toto.TileAt(row, 0).Symbol === this._toto.TileAt(row, 1).Symbol &&
+                this._toto.TileAt(row, 1).Symbol === this._toto.TileAt(row, 2).Symbol
             ) {
-                return this._toto.TileAt(0, 0)!.Symbol;
+                return this._toto.TileAt(row, 0).Symbol;
             }
         }
-
-        if (
-            this._toto.TileAt(1, 0)!.Symbol != ' ' &&
-            this._toto.TileAt(1, 1)!.Symbol != ' ' &&
-            this._toto.TileAt(1, 2)!.Symbol != ' '
-        ) {
-            if (
-                this._toto.TileAt(1, 0)!.Symbol == this._toto.TileAt(1, 1)!.Symbol &&
-                this._toto.TileAt(1, 2)!.Symbol == this._toto.TileAt(1, 1)!.Symbol
-            ) {
-                return this._toto.TileAt(1, 0)!.Symbol;
-            }
-        }
-
-        if (
-            this._toto.TileAt(2, 0)!.Symbol != ' ' &&
-            this._toto.TileAt(2, 1)!.Symbol != ' ' &&
-            this._toto.TileAt(2, 2)!.Symbol != ' '
-        ) {
-            if (
-                this._toto.TileAt(2, 0)!.Symbol == this._toto.TileAt(2, 1)!.Symbol &&
-                this._toto.TileAt(2, 2)!.Symbol == this._toto.TileAt(2, 1)!.Symbol
-            ) {
-                return this._toto.TileAt(2, 0)!.Symbol;
-            }
-        }
-
         return ' ';
     }
 }
