d3fbe3612e688708dfc3855f657835482450a50c
ElAbdos <153218250+ElAbdos@users.noreply.github.com>
2025-09-30 09:25:31 +0200

    Ajout de commentaire javadoc utile a chaque méthode

--- src/12_RefactoringGolf/exo2/kata.ts
+++ src/12_RefactoringGolf/exo2/kata.ts
@@ -4,6 +4,13 @@ export class Game {
     private _lastSymbol = ' ';
     private _board: Board = new Board();
 
+    /**
+     * Play a move
+     * @param symbol
+     * @param x
+     * @param y
+     * @constructor
+     */
     public Play(symbol: string, x: number, y: number): void {
         this.validateFirstMove(symbol);
         this.validatePlayer(symbol);
@@ -12,6 +19,11 @@ export class Game {
         this.updateBoard(symbol, x, y);
     }
 
+    /**
+     * Validate that the first move is made by player 'X'
+     * @param player
+     * @private
+     */
     private validateFirstMove(player: string) {
         if (this._lastSymbol == ' ') {
             if (player == 'O') {
@@ -20,26 +32,54 @@ export class Game {
         }
     }
 
+    /**
+     * Validate that the next player is not the same as the last one
+     * @param player
+     * @private
+     */
     private validatePlayer(player: string) {
         if (player == this._lastSymbol) {
             throw new Error('Invalid next player');
         }
     }
 
+    /**
+     * Validate that the position is empty
+     * @param x
+     * @param y
+     * @private
+     */
     private validatePositionIsEmpty(x: number, y: number) {
         if (this._board.TileAt(x, y).Symbol != ' ') {
             throw new Error('Invalid position');
         }
     }
 
+    /**
+     * Update the last player
+     * @param player
+     * @private
+     */
     private updateLastPlayer(player: string) {
         this._lastSymbol = player;
     }
 
+    /**
+     * Update the board with the player's move
+     * @param player
+     * @param x
+     * @param y
+     * @private
+     */
     private updateBoard(player: string, x: number, y: number) {
         this._board.AddTileAt(player, x, y);
     }
 
+    /**
+     * Check if a row is full
+     * @param row
+     * @private
+     */
     private isRowFull(row: number): boolean {
         return (
             this._board.TileAt(row, 0)!.Symbol != ' ' &&
@@ -48,6 +88,11 @@ export class Game {
         );
     }
 
+    /**
+     * Check if a row is full with the same symbol
+     * @param row
+     * @private
+     */
     private isRowFullWithSameSymbol(row: number): boolean {
         return (
             this._board.TileAt(row, 0)!.Symbol == this._board.TileAt(row, 1)!.Symbol &&
@@ -56,6 +101,10 @@ export class Game {
     }
 
 
+    /**
+     * Get the winner of the game
+     * @constructor
+     */
     public Winner(): string {
         for (let row = 0; row < 3; row++) {
             if (this.isRowFull(row) && this.isRowFullWithSameSymbol(row)) {
@@ -72,6 +121,9 @@ interface Tile {
     Symbol: string;
 }
 
+/**
+ * Class representing the Tic-Tac-Toe board
+ */
 class Board {
     private _plays: Tile[] = [];
 
@@ -84,10 +136,23 @@ class Board {
         }
     }
 
+    /**
+     * Get the tile at the given position
+     * @param x
+     * @param y
+     * @constructor
+     */
     public TileAt(x: number, y: number): Tile {
         return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
     }
 
+    /**
+     * Add a tile at the given position
+     * @param symbol
+     * @param x
+     * @param y
+     * @constructor
+     */
     public AddTileAt(symbol: string, x: number, y: number): void {
         this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
     }
