050359a8518718fe3c64d3dd36fc7544d6e8d5ee
ElAbdos <abdeloffipro@gmail.com>
2025-11-24 08:56:20 +0100

    Add comments to Board and Kata methods

--- src/12_RefactoringGolf/exo3/board.ts
+++ src/12_RefactoringGolf/exo3/board.ts
@@ -12,10 +12,23 @@ export class Board {
         }
     }
 
+    /**
+     * Returns the tile at the given coordinates
+     * @param x
+     * @param y
+     * @constructor
+     */
     public TileAt(x: number, y: number): Tile {
         return this._plays.find((t) => t.X == x && t.Y == y)!;
     }
 
+    /**
+     * Adds a tile at the given coordinates
+     * @param symbol
+     * @param x
+     * @param y
+     * @constructor
+     */
     public AddTileAt(symbol: string, x: number, y: number): void {
         this._plays.find((t) => t.X == x && t.Y == y)!.Symbol = symbol;
     }
--- src/12_RefactoringGolf/exo3/kata.ts
+++ src/12_RefactoringGolf/exo3/kata.ts
@@ -5,15 +5,26 @@ export class Game {
   private _lastSymbol = EMPTY_SYMBOL;
   private _board: Board = new Board();
 
+  /**
+   * Allows a player to play at the given coordinates
+   * @param symbol
+   * @param x
+   * @param y
+   * @constructor
+   */
   public Play(symbol: string, x: number, y: number): void {
     this.validateFirstMove(symbol);
     this.validatePlayer(symbol);
     this.validatePositionIsEmpty(x, y);
-
     this.updateLastPlayer(symbol);
     this.updateBoard(symbol, x, y);
   }
 
+  /**
+   * Validates that the first move is made by player X
+   * @param player
+   * @private
+   */
   private validateFirstMove(player: string) {
     if (this._lastSymbol === EMPTY_SYMBOL) {
       if (player === PLAYER_O) {
@@ -22,26 +33,53 @@ export class Game {
     }
   }
 
+  /**
+   * Validates that the player is not the same as the last player
+   * @param player
+   * @private
+   */
   private validatePlayer(player: string) {
     if (player === this._lastSymbol) {
       throw new Error('Invalid next player');
     }
   }
 
+  /**
+   * Validates that the position is empty
+   * @param x
+   * @param y
+   * @private
+   */
   private validatePositionIsEmpty(x: number, y: number) {
     if (this._board.TileAt(x, y).Symbol !== EMPTY_SYMBOL) {
       throw new Error('Invalid position');
     }
   }
 
+  /**
+   * Updates the last player
+   * @param player
+   * @private
+   */
   private updateLastPlayer(player: string) {
     this._lastSymbol = player;
   }
 
+  /**
+   * Updates the board with the player's move
+   * @param player
+   * @param x
+   * @param y
+   * @private
+   */
   private updateBoard(player: string, x: number, y: number) {
     this._board.AddTileAt(player, x, y);
   }
 
+  /**
+   * Returns the winner of the game
+   * @constructor
+   */
   public Winner(): string {
     for (let row = 0; row < BOARD_SIZE; row++) {
       if (this.isRowFull(row) && this.isRowFullWithSameSymbol(row)) {
@@ -51,6 +89,11 @@ export class Game {
     return EMPTY_SYMBOL;
   }
 
+  /**
+   * Checks if a row is full
+   * @param row
+   * @private
+   */
   private isRowFull(row: number): boolean {
     for (let col = 0; col < BOARD_SIZE; col++) {
       if (this._board.TileAt(row, col)!.Symbol === EMPTY_SYMBOL) {
@@ -60,6 +103,11 @@ export class Game {
     return true;
   }
 
+  /**
+   * Checks if a row is full of the same symbol
+   * @param row
+   * @private
+   */
   private isRowFullWithSameSymbol(row: number): boolean {
     const first = this._board.TileAt(row, 0)!.Symbol;
     for (let col = 1; col < BOARD_SIZE; col++) {
