40c80c5bc3a91bc042f7f4f7bcf6b2f8f437b867
ElAbdos <abdeloffipro@gmail.com>
2025-12-08 09:09:24 +0100

    Faire passer un objet tile à game.updatebord pour regroupe des parametres

--- src/12_RefactoringGolf/exo07/kata.ts
+++ src/12_RefactoringGolf/exo07/kata.ts
@@ -20,7 +20,7 @@ export class Game {
         this.validatePositionIsEmpty(x, y);
 
         this.updateLastPlayer(player);
-        this.updateBoard(player, x, y);
+        this.updateBoard(new Tile(x, y, player));
     }
 
     public Winner(): string {
@@ -51,8 +51,8 @@ export class Game {
         this._lastPlayer = player;
     }
 
-    private updateBoard(player: string, x: number, y: number) {
-        this._board.AddTileAt(new Tile(x, y, player));
+    private updateBoard(tile: Tile) {
+        this._board.AddTileAt(tile);
     }
 }
 
