a4f3501d23386d27f05547be9a2eb6e090badf54
ElAbdos <abdeloffipro@gmail.com>
2025-12-08 08:11:03 +0100

    Centraliser les constantes dans une seule et unique fichier constants.ts

--- src/12_RefactoringGolf/exo5/constants.ts
+++ src/12_RefactoringGolf/exo5/constants.ts
@@ -4,6 +4,5 @@ export const THIRD_ROW = 2;
 export const FIRST_COLUMN = 0;
 export const SECOND_COLUMN = 1;
 export const THIRD_COLUMN = 2;
-
 export const PLAYER_O = 'O';
 export const EMPTY_PLAY = ' ';
